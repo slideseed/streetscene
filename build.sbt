4959406fd60754682e35595a805ae890e086fe5c
Nate Kidwell <nate@slideseed.com>
2014-01-04 14:32:03 -0500

    initial commit

---
+++ build.sbt
@@ -0,0 +1,7 @@
+organization := "io.pendulum"
+
+name := "wmemllo"
+
+version := "0.1-SNAPSHOT"
+
+libraryDependencies += "org.scala-sbt" % "command" % "0.12.4"
---
+++ src/main/scala/RunMem.scala
@@ -0,0 +1,60 @@
+import scala.util.Random
+object World {
+  val random = new Random()
+  def rangeFrom(low: Int, high: Int) =
+    random.nextInt(1 + (high - low)) + low
+
+  def oneIn(num: Int, obj:Any):Option[Any] =
+    if (random.nextInt(num) == 0)
+      Some(obj)
+    else
+      None
+
+  class Store {
+
+  }
+
+
+  class Person {
+
+  }
+
+  class Dog {
+
+  }
+
+  class Fire {
+
+
+  }
+
+  def yesNo = random.nextBoolean
+
+
+  class Wall(building: Building) {
+    val windowCount = rangeFrom(2, 4)
+    val fireEscape = yesNo
+    val store = oneIn(4, new Store())
+    override def toString = "Windows: " + windowCount + " || Fire Escape: " + fireEscape
+  }
+
+  class Building(val number: Int) {
+    val stories = rangeFrom(3, 6)
+    val front = new Wall(this)
+    val side = new Wall(this)
+    override def toString:String = "Building number " + number + "\nNumber of stories: " + stories + "\nSide: " + side + "\nFront: " + front
+  }
+
+  class Scene(buildingCount: Int = 3) {
+    val buildings: List[Building] = (for {
+      i <- 1 to buildingCount
+    } yield (new Building(i))).toList
+
+
+    override def toString = buildings mkString "\n\n"
+  }
+}
+
+object RunMem {
+  def main(args: Array[String]) = print(new World.Scene())
+}
